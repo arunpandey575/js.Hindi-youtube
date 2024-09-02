5101930d0b5294b40b0d9cf743fbb2ed33497a38
arunpandey575 <arunpandey916162@gmail.com>
2024-09-02 19:27:29 +0000

    container config done

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -9,6 +9,24 @@ console.log(`Hello my name is ${name} and my repocount is ${repocount}`);
 //dollor and middle bracket is use for inject
 // Ii is Called string interpullation
 
-const gameName = new String('ARUN')
+const gameName = new String('ARUN-EX')
 console.log(gameName[0]);
 console.log(gameName.__proto__);
+console.log(gameName.length);
+console.log(gameName.toUpperCase);
+console.log(gameName.charAt(3));
+console.log(gameName.indexOf('R'));
+const newString = gameName.substring(0,4)
+console.log(newString);
+
+const anotherString = gameName.slice(-7,3)
+console.log(anotherString);
+
+const newStringOne = " Hitesh"
+console.log(newString);
+console.log(newString.trim);
+
+const url = "https://hitesh.com/hitesh%20choudry"
+console.log(url.replace('%20','-'));
+console.log(url.includee('sunder'));
+console.log(url.split('-'));
